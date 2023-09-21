1f99d376359157a5a55c6b75f55e9585723812d1
Luis Weich <luis.weich.de@gmail.com>
2023-09-21 15:42:54 +0200

    dwm: update shortcuts

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -64,42 +64,24 @@ static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } }, // spawn dmenu prompt
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } }, // spawn terminal
+	{ MODKEY|ShiftMask,             XK_k,      killclient,     {0} }, // kill current window
+	{ MODKEY|ShiftMask,             XK_Left,   focusstack,     {.i = -1 } }, // switch window backward
+	{ MODKEY|ShiftMask,             XK_Right,  focusstack,     {.i = +1 } }, // switch window forward
+	{ MODKEY|ShiftMask,             XK_Up,     zoom,           {0} }, // window as master
+	{ MODKEY|ShiftMask,             XK_minus,  setmfact,       {.f = -0.05} }, // subtract master space
+	{ MODKEY|ShiftMask,             XK_plus,   setmfact,       {.f = +0.05} }, // add master space
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} }, // toggle floating
+	TAGKEYS(                        XK_1,                      0),
+	TAGKEYS(                        XK_2,                      1),
+	TAGKEYS(                        XK_3,                      2),
+	TAGKEYS(                        XK_4,                      3),
+	TAGKEYS(                        XK_5,                      4),
+	TAGKEYS(                        XK_6,                      5),
+	TAGKEYS(                        XK_7,                      6),
+	TAGKEYS(                        XK_8,                      7),
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} }
 };
 
 /* button definitions */

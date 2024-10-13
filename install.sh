45d6bd1d521f2e813dfeb6328ee129531fa84b92
Lugawe <luis.weich.de@gmail.com>
2024-10-13 17:16:21 +0200

    install.sh update

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "Install packages..."
 
-sudo pacman -S git base-devel ttf-hack-nerd ttf-font-awesome alacritty hyprland hyprpaper waybar
+sudo pacman -S git base-devel ttf-hack-nerd ttf-font-awesome alacritty hyprland hypridle hyprlock hyprpaper waybar

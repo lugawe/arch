e436e9d6334708cc0e67e3c8d7dfd435f9ba484b
Luis Weich <72873134+lugawe@users.noreply.github.com>
2025-06-15 17:19:05 +0200

    Update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,17 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 set fish_greeting ""
 
-abbr --add "ad" "alacritty & disown"
+abbr --add ad "alacritty & disown"
 
 for file in $__fish_config_dir/conf.d/*.fish
     source $file
 end
 
-set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    if not set -q TMUX
+        exec tmux
+    end
+end
 
+set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 starship init fish | source
